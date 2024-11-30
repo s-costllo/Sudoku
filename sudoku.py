ba691e58e06e42c1716dd647db9899340ec06dc1
s-costllo <shaylen.costello@ufl.edu>
2024-11-29 21:45:46 -0500

    updated some parts integrating other file

--- sudoku.py
+++ sudoku.py
@@ -1,11 +1,12 @@
 import pygame, sys
+from sudoku_generator import *
 
 def draw_game_start(screen):
     start_title_font = pygame.font.Font("LEMONMILK-Medium.otf", 70)
     button_font = pygame.font.Font("LEMONMILK-Medium.otf", 50)
 
-    # screen.blit(bg, (0, 0))
-    screen.fill("black")
+    screen.blit(bg, (0, 0))
+    #screen.fill("black")
 
     title_surface = start_title_font.render("Sudoku", 1, "white")
     title_rectangle = title_surface.get_rect(
@@ -57,8 +58,8 @@ def draw_game_start(screen):
         pygame.display.update()
 
 def draw_game(screen):
-    #screen.blit(bg, (0, 0))
-    screen.fill("black")
+    screen.blit(bg, (0, 0))
+    #screen.fill("black")
     board = pygame.Surface((603, 603))
     board.fill("white")
     screen.blit(board, (397,0))
@@ -84,21 +85,23 @@ def draw_game(screen):
         center=(397 // 2 , 603 // 2 - 175))
     screen.blit(reset_surface, reset_rectangle)
     restart_rectangle = restart_surface.get_rect(
-        center=(397 // 2, 603 // 2 ))
+        center=(397 // 2, 603 // 2 + 5))
     screen.blit(restart_surface, restart_rectangle)
     exit_rectangle = exit_surface.get_rect(
         center=(397 // 2, 603 // 2 + 175))
     screen.blit(exit_surface, exit_rectangle)
 
     #board lines
+    '''
     for row in range(0, 10, 1):
         pygame.draw.line(screen, (0, 0, 0), (397, row * 67), (1000, row * 67))
     for col in range(0, 10, 1):
-        pygame.draw.line(screen, (0, 0, 0), (col *67 + 397, 0), (col * 67+397, 603))
+        pygame.draw.line(screen, (0, 0, 0), (col * 67 + 397, 0), (col * 67 + 397, 603))
     for row in range(0, 10, 3):
-        pygame.draw.line(screen, (0, 0, 0), (397, row * 67), (1000, row * 67),5)
+        pygame.draw.line(screen, (0, 0, 0), (397, row * 67), (1000, row * 67), 5)
     for col in range(0, 10, 3):
-        pygame.draw.line(screen, (0, 0, 0), (col * 67 + 397, 0), (col * 67 + 397, 603),5)
+        pygame.draw.line(screen, (0, 0, 0), (col * 67 + 397, 0), (col * 67 + 397, 603), 5)
+        '''
 
     #sample number
     number_surface = number_font.render("3", 1, "black")
@@ -106,6 +109,7 @@ def draw_game(screen):
         center=(431, 33))
     screen.blit(number_surface, number_rectangle)
 
+    '''
     #running
     while True:
         for event in pygame.event.get():
@@ -119,9 +123,11 @@ def draw_game(screen):
                 elif exit_rectangle.collidepoint(event.pos):
                     return 3
         pygame.display.update()
+    '''
 
 def draw_game_over(screen):
-    screen.fill("black")
+    #screen.fill("black")
+    screen.blit(bg, (0, 0))
     start_title_font = pygame.font.Font("LEMONMILK-Medium.otf", 70)
     button_font = pygame.font.Font("LEMONMILK-Medium.otf", 50)
 
@@ -138,6 +144,7 @@ def draw_game_over(screen):
         center=(1000 // 2, 603 // 2 + 175))
     screen.blit(exit_surface, exit_rectangle)
 
+    '''
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -146,9 +153,11 @@ def draw_game_over(screen):
                 if exit_rectangle.collidepoint(event.pos):
                     sys.exit()
         pygame.display.update()
+    '''
 
 def draw_game_won(screen):
-    screen.fill("black")
+    #screen.fill("black")
+    screen.blit(bg, (0, 0))
     start_title_font = pygame.font.Font("LEMONMILK-Medium.otf", 70)
     button_font = pygame.font.Font("LEMONMILK-Medium.otf", 50)
 
@@ -174,7 +183,6 @@ def draw_game_won(screen):
                     return 2
         pygame.display.update()
 
-
 if __name__ == '__main__':
 
     pygame.init()
@@ -182,7 +190,34 @@ if __name__ == '__main__':
     bg = pygame.image.load("TL.png")
     # bg and dimensions subject to change, just using it for reference right now
 
+
     while True:
+        sudoku = SudokuGenerator(9, 30)
+        sudoku.fill_diagonal()
+        sudoku.fill_values()
+        x = sudoku.fill_remaining(0,3)
+        print(x)
+        sudoku.print_board()
+
+        diff = draw_game_start(screen)
+        board = Board(9, 9, screen, diff)
+
+        screen.blit(bg, (0, 0))
+        draw_game(screen)
+        board.draw()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if reset_rectangle.collidepoint(event.pos):
+                        x = 1
+                    elif restart_rectangle.collidepoint(event.pos):
+                        x = 2
+                    elif exit_rectangle.collidepoint(event.pos):
+                        sys.exit()
+            pygame.display.update()
+        '''
         diff = draw_game_start(screen)
         if diff == 1:
             boardOption = draw_game(screen)
@@ -190,13 +225,15 @@ if __name__ == '__main__':
             boardOption = draw_game(screen)
         if diff == 3:
             boardOption = draw_game(screen)
-
+        
+    
         if boardOption == 1:
+            #just testing game won and game lost screens
             draw_game_won(screen)
         elif boardOption == 2:
             draw_game_over(screen)
         elif boardOption == 3:
             sys.exit()
-
+        '''
 
 
